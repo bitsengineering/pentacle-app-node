1d88aeb7866dfa4318bd435ed02822f540c615f9
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-08-27 18:50:31 +0300

    index:model files import export

--- src/model/index.ts
+++ src/model/index.ts
@@ -1,21 +1,27 @@
 import Decoder from "./Decoder";
 import Encoder from "./Encoder";
 import Header from "./Header";
-import Headers from "./Headers";
+import GetHeaders from "./GetHeaders";
 import Inventory from "./Inventory";
 import PayloadReference from "./PayloadReference";
 import PingPong from "./PingPong";
 import ServiceBit from "./ServiceBit";
 import Version from "./Version";
+import Message from "./Message";
+import Block from "./Block";
+import Transaction from "./Transaction";
 
 export {
   Decoder,
   Encoder,
   Header,
-  Headers,
+  GetHeaders,
   Inventory,
   PayloadReference,
   PingPong,
   ServiceBit,
   Version,
+  Message,
+  Block,
+  Transaction,
 };
