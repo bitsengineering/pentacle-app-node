b593fa7dba7ac5db17f9e399d4c772fca15a87a2
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-03 17:27:41 +0300

    index: Peer and Peers connection test functions

--- src/index.js
+++ src/index.js
@@ -3,67 +3,60 @@ exports.__esModule = true;
 // import Peer from "./peer";
 // import PeerGroup from "./peerGroup.js";
 var lib_1 = require("./lib");
+var getBlocks = function () {
+    peer.getBlocks([Buffer.from(GENESIS_BLOCK_HASH, "hex").reverse()], {}, function (err, blocks) {
+        if (err == null) {
+            console.log("getBlocks response for Genesis block:");
+            blocks.forEach(function (block) {
+                // console.log(block);
+                block.transactions.forEach(function (transaction) {
+                    // console.log(transaction);
+                });
+            });
+        }
+        else {
+            console.error("getBlocks response error:");
+            console.error(err);
+        }
+    });
+};
+var getHeaders = function () {
+    peer.getHeaders([Buffer.from(GENESIS_BLOCK_HASH, "hex").reverse()], {}, function (err, headers) {
+        if (err == null) {
+            console.log("getHeaders response for Genesis block:");
+            console.log(headers.length);
+            console.log(headers.slice(0, 10));
+            console.log(JSON.stringify(headers));
+        }
+        else {
+            console.error("getHeaders response error:");
+            console.error(err);
+        }
+    });
+};
+var getTransactions = function () {
+    peer.getTransactions(Buffer.from("000000000000000000028237ea0c92173613601276ae0182b8fd04388fe3fc6a", "hex").reverse(), [
+        Buffer.from("c72af2bd4827b54e4eef64a01a1e8fa29601c459c1e37dd9ab76dd118338e7bd", "hex").reverse(),
+    ], {}, function (_err, transactions) {
+        console.log("err", _err);
+        console.log("transactions", transactions);
+        transactions === null || transactions === void 0 ? void 0 : transactions.map(function (transaction) {
+            console.log("transaction", transaction);
+        });
+    });
+};
 /* ****** */
 //PEER GROUP CONNECTION
-// const GENESIS_BLOCK_HASH =
-//   "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
-// const peerGroup = new Peers(params);
-// peerGroup.on("peer", (peer: any) => {
-//   console.log("peer", peer);
-//   // console.log("connected to peer", peer.socket.remoteAddress);
-//   // console.log(peer);
-//   // send/receive messages
-//   peer.once("pong", () => {
-//     // console.log("pong");ƒ
-//   });
-//   const randomBytes = pseudoRandomBytes(8);
-//   peer.send("ping", { nonce: randomBytes });
-//   // console.log("ping");
-// });
-// peerGroup.on("connect", () => {
+var GENESIS_BLOCK_HASH = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
+// const peer = new Peers(params);
+// peer.on("connect", () => {
 //   console.log("connected");
-//   // testFunction;
-//   testFunction2();
+//   // getBlocks();
+//   getHeaders();
+//   getTransactions();
 // });
 // // create connections to peers
-// peerGroup.connect();
-// // const testFunction = () => {
-// //   peerGroup.getBlocks(
-// //     [Buffer.from(GENESIS_BLOCK_HASH, "hex").reverse()],
-// //     {},
-// //     (err: Error, blocks: Array<Block>) => {
-// //       if (err == null) {
-// //         console.log("getBlocks response for Genesis block:");
-// //         blocks.forEach((block: Block) => {
-// //           // console.log(block);
-// //           block.transactions.forEach((transaction: Transaction) => {
-// //             // console.log(transaction);
-// //           });
-// //         });
-// //       } else {
-// //         console.error("getBlocks response error:");
-// //         console.error(err);
-// //       }
-// //     }
-// //   );
-// // };
-// const testFunction2 = () => {
-//   peerGroup.getHeaders(
-//     [Buffer.from(GENESIS_BLOCK_HASH, "hex").reverse()],
-//     {},
-//     (err: any, headers: any[]) => {
-//       if (err == null) {
-//         console.log("getHeaders response for Genesis block:");
-//         // console.log(headers.length);
-//         // console.log(headers.slice(0, 10));
-//         // console.log(JSON.stringify(headers));
-//       } else {
-//         console.error("getHeaders response error:");
-//         console.error(err);
-//       }
-//     }
-//   );
-// };
+// peer.connect();
 /* ****** */
 /* ****** */
 //PEER CONNECTION
@@ -71,78 +64,15 @@ var net = require("net");
 var socket = net.connect({ port: 8333, host: "seed.bitcoinstats.com" }, function () {
     var peer = new lib_1.Peer({ magic: 0xd9b4bef9, defaultPort: 8333 }, { socket: socket });
     peer.once("ready", function () {
-        /* ****** */
         // GET BLOCKS
-        // peer.getBlocks(
-        //   [
-        //     Buffer.from(
-        //       "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
-        //       "hex"
-        //     ).reverse(),
-        //   ],
-        //   {},
-        //   (_err: Error | null, blocks?: Array<Block>) => {
-        //     console.log("err", _err);
-        //     // console.log("blocks", blocks);
-        //     blocks?.forEach((block: Block) => {
-        //       console.log(block);
-        //       block.transactions.forEach((transaction: Transaction) => {
-        //         console.log(transaction);
-        //         const a = utils.toHexString(transaction.ins[0].hash);
-        //         console.log("hashhh", a);
-        //       });
-        //     });
-        //   }
-        // );
-        //       /* ****** */
-        //       /* ****** */
-        //       //GET HEADERS
-        //       // peer.getHeaders(
-        //       //   [
-        //       //     Buffer.from(
-        //       //       "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
-        //       //       "hex"
-        //       //     ).reverse(),
-        //       //   ],
-        //       //   {},
-        //       //   (_err: Error | null, headers?: Array<Header>) => {
-        //       //     console.log("err", _err);
-        //       //     console.log("headers", headers);
-        //       //   }
-        //       // );
-        //       /* ****** */
-        //       /* ****** */
-        //       // GET TRANSACTIONS
-        peer.getTransactions(Buffer.from("0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002", "hex").reverse(), [
-            Buffer.from("43fff6eecfb04aca9c7bcc525fcf80f8704f7b603bb26af834432229720af5ba"),
-            // Buffer.from(
-            //   "2eeea8bd0f1ca3d67a72c985f7f47a98266b62118319a5724455c26931280771",
-            //   "hex"
-            // ).reverse(),
-            // Buffer.from(
-            //   "14d01ef4e87cbbc01ee666344b65dad5f541ca9fb9a849a5ddb4f818ea0ffa83",
-            //   "hex"
-            // ).reverse(),
-            // Buffer.from(
-            //   "ac142e8f05f2b1b668fcb8e74089d59761788c92283cdceb89edcb04e37f6f34",
-            //   "hex"
-            // ).reverse(),
-            // Buffer.from(
-            //   "c8ac6c70e76ef7cb6dc78ec67e1df015da9943542097f6aecfe4dcd5f396bba2",
-            //   "hex"
-            // ).reverse(),
-            // Buffer.from(
-            //   "07d7638ad27fef32db4fa3e2e490d0c5601ad5b22e022e72a33073de1f9eba91",
-            //   "hex"
-            // ).reverse(),
-        ], {}, function (_err, transactions) {
-            console.log("err", _err);
-            console.log("transactions", transactions);
-            transactions === null || transactions === void 0 ? void 0 : transactions.map(function (transaction) {
-                console.log("transaction", transaction);
-            });
-        });
-        //       /* ****** */
+        getBlocks();
+        /* ****** */
+        //GET HEADERS
+        // getHeaders();
+        /* ****** */
+        // GET TRANSACTIONS
+        // getTransactions();
+        /* ****** */
     });
 });
 /* ****** */
--- src/index.ts
+++ src/index.ts
@@ -9,78 +9,59 @@ import { Block, Header, Transaction } from "./model";
 /* ****** */
 //PEER GROUP CONNECTION
 
-// const GENESIS_BLOCK_HASH =
-//   "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
+const GENESIS_BLOCK_HASH =
+  "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f";
 
-// const peerGroup = new Peers(params);
+const peerGroup = new Peers(params);
 
-// peerGroup.on("peer", (peer: any) => {
-//   console.log("peer", peer);
+peerGroup.on("connect", () => {
+  console.log("connected");
+  // getPeersBlocks();
+  getPeersHeaders();
+});
 
-//   // console.log("connected to peer", peer.socket.remoteAddress);
+// create connections to peers
+peerGroup.connect();
 
-//   // console.log(peer);
+const getPeersBlocks = () => {
+  peerGroup.getBlocks(
+    [Buffer.from(GENESIS_BLOCK_HASH, "hex").reverse()],
+    {},
+    (err: Error, blocks: Array<Block>) => {
+      if (err == null) {
+        console.log("getBlocks response for Genesis block:");
 
-//   // send/receive messages
-//   peer.once("pong", () => {
-//     // console.log("pong");ƒ
-//   });
+        blocks.forEach((block: Block) => {
+          // console.log(block);
+          block.transactions.forEach((transaction: Transaction) => {
+            // console.log(transaction);
+          });
+        });
+      } else {
+        console.error("getBlocks response error:");
+        console.error(err);
+      }
+    }
+  );
+};
 
-//   const randomBytes = pseudoRandomBytes(8);
-//   peer.send("ping", { nonce: randomBytes });
-//   // console.log("ping");
-// });
-
-// peerGroup.on("connect", () => {
-//   console.log("connected");
-
-//   // testFunction;
-//   testFunction2();
-// });
-
-// // create connections to peers
-// peerGroup.connect();
-
-// // const testFunction = () => {
-// //   peerGroup.getBlocks(
-// //     [Buffer.from(GENESIS_BLOCK_HASH, "hex").reverse()],
-// //     {},
-// //     (err: Error, blocks: Array<Block>) => {
-// //       if (err == null) {
-// //         console.log("getBlocks response for Genesis block:");
-
-// //         blocks.forEach((block: Block) => {
-// //           // console.log(block);
-// //           block.transactions.forEach((transaction: Transaction) => {
-// //             // console.log(transaction);
-// //           });
-// //         });
-// //       } else {
-// //         console.error("getBlocks response error:");
-// //         console.error(err);
-// //       }
-// //     }
-// //   );
-// // };
-
-// const testFunction2 = () => {
-//   peerGroup.getHeaders(
-//     [Buffer.from(GENESIS_BLOCK_HASH, "hex").reverse()],
-//     {},
-//     (err: any, headers: any[]) => {
-//       if (err == null) {
-//         console.log("getHeaders response for Genesis block:");
-
-//         // console.log(headers.length);
-//         // console.log(headers.slice(0, 10));
-//         // console.log(JSON.stringify(headers));
-//       } else {
-//         console.error("getHeaders response error:");
-//         console.error(err);
-//       }
-//     }
-//   );
-// };
+const getPeersHeaders = () => {
+  peerGroup.getHeaders(
+    [Buffer.from(GENESIS_BLOCK_HASH, "hex").reverse()],
+    {},
+    (err: any, headers: any[]) => {
+      if (err == null) {
+        console.log("getHeaders response for Genesis block:");
+        console.log(headers.length);
+        console.log(headers.slice(0, 10));
+        console.log(JSON.stringify(headers));
+      } else {
+        console.error("getHeaders response error:");
+        console.error(err);
+      }
+    }
+  );
+};
 
 /* ****** */
 
@@ -94,77 +75,65 @@ const socket: Socket = net.connect(
     const peer = new Peer({ magic: 0xd9b4bef9, defaultPort: 8333 }, { socket });
 
     peer.once("ready", () => {
-      /* ****** */
       // GET BLOCKS
-      // peer.getBlocks(
-      //   [
-      //     Buffer.from(
-      //       "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
-      //       "hex"
-      //     ).reverse(),
-      //   ],
-      //   {},
-      //   (_err: Error | null, blocks?: Array<Block>) => {
-      //     console.log("err", _err);
-      //     // console.log("blocks", blocks);
-      //     blocks?.forEach((block: Block) => {
-      //       console.log(block);
-      //       block.transactions.forEach((transaction: Transaction) => {
-      //         console.log(transaction);
-      //         const a = utils.toHexString(transaction.ins[0].hash);
-      //         console.log("hashhh", a);
-      //       });
-      //     });
-      //   }
-      // );
-      //       /* ****** */
-      //       /* ****** */
-      //       //GET HEADERS
-      //       // peer.getHeaders(
-      //       //   [
-      //       //     Buffer.from(
-      //       //       "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
-      //       //       "hex"
-      //       //     ).reverse(),
-      //       //   ],
-      //       //   {},
-      //       //   (_err: Error | null, headers?: Array<Header>) => {
-      //       //     console.log("err", _err);
-      //       //     console.log("headers", headers);
-      //       //   }
-      //       // );
-      //       /* ****** */
-      //       /* ****** */
-      //       // GET TRANSACTIONS
+      getPeerBlocks();
 
+      //GET HEADERS
+      getPeerHeaders();
+
+      // GET TRANSACTIONS
+      getPeerTransactions();
+    });
+
+    const getPeerHeaders = () => {
+      peer.getHeaders(
+        [
+          Buffer.from(
+            "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
+            "hex"
+          ).reverse(),
+        ],
+        {},
+        (_err: Error | null, headers?: Array<Header>) => {
+          console.log("err", _err);
+          console.log("headers", headers);
+        }
+      );
+    };
+
+    const getPeerBlocks = () => {
+      peer.getBlocks(
+        [
+          Buffer.from(
+            "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
+            "hex"
+          ).reverse(),
+        ],
+        {},
+        (_err: Error | null, blocks?: Array<Block>) => {
+          console.log("err", _err);
+          // console.log("blocks", blocks);
+          blocks?.forEach((block: Block) => {
+            console.log(block);
+            block.transactions.forEach((transaction: Transaction) => {
+              console.log(transaction);
+              const a = utils.toHexString(transaction.ins[0].hash);
+              console.log("hashhh", a);
+            });
+          });
+        }
+      );
+    };
+
+    const getPeerTransactions = () => {
       peer.getTransactions(
         Buffer.from(
-          "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
+          "000000000000000000028237ea0c92173613601276ae0182b8fd04388fe3fc6a",
           "hex"
         ).reverse(),
         [
           Buffer.from(
-            "43fff6eecfb04aca9c7bcc525fcf80f8704f7b603bb26af834432229720af5ba",
-            "hex"
-          ).reverse(),
-          Buffer.from(
-            "2eeea8bd0f1ca3d67a72c985f7f47a98266b62118319a5724455c26931280771",
-            "hex"
-          ).reverse(),
-          Buffer.from(
-            "14d01ef4e87cbbc01ee666344b65dad5f541ca9fb9a849a5ddb4f818ea0ffa83",
-            "hex"
-          ).reverse(),
-          Buffer.from(
-            "ac142e8f05f2b1b668fcb8e74089d59761788c92283cdceb89edcb04e37f6f34",
-            "hex"
-          ).reverse(),
-          Buffer.from(
-            "c8ac6c70e76ef7cb6dc78ec67e1df015da9943542097f6aecfe4dcd5f396bba2",
-            "hex"
-          ).reverse(),
-          Buffer.from(
-            "07d7638ad27fef32db4fa3e2e490d0c5601ad5b22e022e72a33073de1f9eba91",
+            "c72af2bd4827b54e4eef64a01a1e8fa29601c459c1e37dd9ab76dd118338e7bd",
             "hex"
           ).reverse(),
         ],
@@ -177,8 +146,7 @@ const socket: Socket = net.connect(
           });
         }
       );
-      //       /* ****** */
-    });
+    };
   }
 );
 
