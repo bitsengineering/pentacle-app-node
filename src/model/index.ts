3250e4b93ae839c209a900ca68ddbb25816bb4c5
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-03 17:27:15 +0300

    model index: import export

--- src/model/index.ts
+++ src/model/index.ts
@@ -10,7 +10,8 @@ import Version from "./Version";
 import Message from "./Message";
 import Block from "./Block";
 import Transaction from "./Transaction";
-import Params from "./Params";
+import PeerParams from "./PeerParams";
+import PeersParams from "./PeersParams";
 import Opts from "./Opts";
 
 export {
@@ -26,6 +27,7 @@ export {
   Message,
   Block,
   Transaction,
-  Params,
+  PeerParams,
+  PeersParams,
   Opts,
 };
