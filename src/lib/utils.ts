4b630a6c6643657a4b1fc748ab59740ce66170eb
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-10 17:10:04 +0300

    utils: getServices func added

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,29 @@
 import { createHash } from "crypto";
+import { ServiceBit } from "../model";
+import { serviceBits } from "./constants";
 
 const encodeHeader = require("bitcoin-protocol").types.header.encode;
 const encodeTx = require("bitcoin-protocol").types.transaction.encode;
 
 const sha256 = (data: any) => createHash("sha256").update(data).digest();
 
-export const hashBlock = (header: any): string => sha256(sha256(encodeHeader(header))).toString("base64");
+export const hashBlock = (header: any): string =>
+  sha256(sha256(encodeHeader(header))).toString("base64");
 
-export const hashTx = (tx: any): string => sha256(sha256(encodeTx(tx))).toString("base64");
+export const hashTx = (tx: any): string =>
+  sha256(sha256(encodeTx(tx))).toString("base64");
 
 // export const toHexString = (byteArray: Uint8Array) => Array.from(byteArray, (byte) => ("0" + (byte & 0xff).toString(16)).slice(-2)).join("");
+
+export const getServices = (buf: Buffer) => {
+  const services: { [key: string]: boolean } = {};
+  serviceBits.forEach((sr: ServiceBit) => {
+    const byteIndex = Math.floor(sr.value / 8);
+    const byte = buf.readUInt32LE(byteIndex);
+    const bitIndex = sr.value % 8;
+    if (byte & (1 << bitIndex)) {
+      services[sr.key] = true;
+    }
+  });
+  return services;
+};
