f4a206625f99d342514f19b59e0d8ada56ab1189
Penguen Umut <uuisik@gmail.com>
2021-09-14 23:17:01 +0300

    Transaction model witness

--- src/model/Transaction.ts
+++ src/model/Transaction.ts
@@ -11,8 +11,10 @@ type In = {
 };
 
 type Transaction = {
-  version: 1;
+  version: 1 | 2;
   ins: Array<In>;
+  marker?: uint8; // witness transaction value = 0
+  flag?: uint8; // witness transaction value = 1
   outs: Array<Out>;
   locktime: 0;
 };
