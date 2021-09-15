ac812cb59cf1e442f0a20da1aaeb89add954bc31
afarukcali <afarukcali@gmail.com>
2021-09-15 15:07:41 +0300

    difficuilty result

--- src/lib/feat.ts
+++ src/lib/feat.ts
@@ -1,20 +1,31 @@
+import { BigInteger } from "big-integer";
+
 const bigInt = require("big-integer");
 
 const currentTarget = 0x1d00ffff;
 
 const twoWeekSec = 1209600;
 
-export const difficuiltyVerify = (prevTimestamp: number, currentTimestamp: number) => {
+export const difficuiltyVerify = (prevTimestamp: number, currentTimestamp: number): BigInteger => {
   // 2 week
   const timeDiff = currentTimestamp - prevTimestamp;
 
   const currentTargetValue = bitsToTarget(currentTarget);
 
-  const multiplyDifference = bigInt(Number(currentTargetValue.value) * timeDiff).value;
+  const timeDiffValue = bigInt(timeDiff);
+
+  const multiplyDifference = currentTargetValue.multiply(timeDiffValue);
+
+  const divResultToTwoWeek = multiplyDifference.divide(bigInt(twoWeekSec));
+
+  const difficultyIndexResult = difficultyIndex(divResultToTwoWeek.toString(16));
+
+  // compare with 1
+  return difficultyIndexResult;
 };
 
-const bitsToTarget = (bitsInput: number): { value: number } => {
-  let bits = bigInt(bitsInput);
+export const bitsToTarget = (bitsInput: number): BigInteger => {
+  let bits: BigInteger = bigInt(bitsInput);
 
   var sign = bits.and(0x00800000).shiftRight(24).toJSNumber();
 
@@ -25,4 +36,13 @@ const bitsToTarget = (bitsInput: number): { value: number } => {
   var target = mantissa.times(Math.pow(-1, sign)).shiftLeft(8 * (exponent - 3));
 
   return target;
+};
+
+export const difficultyIndex = (target: string): BigInteger => {
+  const limit = bigInt("00000000FFFF0000000000000000000000000000000000000000000000000000", 16);
+  const targetValue = bigInt(target, 16);
+
+  const difficultyIndex: BigInteger = limit.divide(targetValue).toJSNumber();
+
+  return difficultyIndex;
 };
