51ad6dadf67b7031ccbf412f00207357c19b3571
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-16 17:19:20 +0300

    Buffer type parameters changed as the string

--- src/index.ts
+++ src/index.ts
@@ -6,33 +6,35 @@ import { hashTx } from "./lib/utils";
 
 const peer = new Peer({ magic: 0xd9b4bef9, defaultPort: 8333 }, {});
 
-const getPeerHeaders = () => {
+const getPeerHeaders = (blockHashes?: string[]): Promise<Header[][]> => {
   console.log("getPeerHeaders");
 
-  const hashes: Buffer[] = [Buffer.from("0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002", "hex").reverse()];
+  const hashes: string[] = blockHashes || ["0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002"];
 
-  peer
+  return peer
     .getHeaders(hashes)
     .then((headerses: Header[][]) => {
       console.log("getPeerHeaders then");
       console.log(headerses.length);
       console.log(headerses[0].length);
       console.log("headers", headerses[0][0].header.version);
+      return headerses;
     })
     .catch((error) => {
       console.log("getPeerHeaders catch");
       console.log(error);
+      throw error;
     });
 };
 
-const getPeersBlocks = () => {
+const getPeerBlocks = (blockHashes?: string[]): Promise<Block[]> => {
   console.log("getPeersBlocks");
 
-  const hashes: Buffer[] = [
-    // Buffer.from("0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002", "hex").reverse(),
-    // Buffer.from("0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002", "hex").reverse(),
-    Buffer.from("6a4690e6ba50e286b8c63c826399a6ac73be3f479f17406cdf90468700000000", "hex"),
-    // Buffer.from("00000000000000000008c80ccd63c1259027d30b96c48feacae19f055b1f1677", "hex").reverse(),
+  const hashes: string[] = blockHashes || [
+    // "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
+    // "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
+    "6a4690e6ba50e286b8c63c826399a6ac73be3f479f17406cdf90468700000000",
+    // "00000000000000000008c80ccd63c1259027d30b96c48feacae19f055b1f1677",
   ];
   const merkle: boolean = false;
 
@@ -43,7 +45,7 @@ const getPeersBlocks = () => {
       // console.log(blocksArray.length);
       // console.log("blocks header.prevHash", blocksArray[0].header.prevHash);
       console.log("blocks transactions[0] budur", blocksArray[0].transactions[0]);
-      return blocksArray[0];
+      return blocksArray;
     })
     .catch((error) => {
       console.log("blocksArray catch");
@@ -52,16 +54,13 @@ const getPeersBlocks = () => {
     });
 };
 
-const getTransactionsById = (buffer?: Buffer, witness?: boolean) => {
-  const hashes: Buffer[] = buffer
-    ? [buffer]
-    : [
-        // Buffer.from("538e7f38afa8c6434191c1693a8b30de7cede3a73883f83e7b07c637f2109005", "hex").reverse(),
-        // Buffer.from("f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16", "hex").reverse(),
-        // Buffer.from("3797c09006aaad367f7342e215820e499bfbb809f042c690fb7a71b8537c0868", "hex").reverse(),
-        // Buffer.from(txid, "hex").reverse(),
-        Buffer.from("1d2362fba0bd11cabdae3e080dad5f0f4db43799052ccaedfe1823baf3b702da", "hex").reverse(),
-      ];
+const getPeerTransactionsByTx = (txids?: string[], witness?: boolean) => {
+  const hashes: string[] = txids || [
+    // "538e7f38afa8c6434191c1693a8b30de7cede3a73883f83e7b07c637f2109005",
+    // "f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16",
+    // "3797c09006aaad367f7342e215820e499bfbb809f042c690fb7a71b8537c0868",
+    "1d2362fba0bd11cabdae3e080dad5f0f4db43799052ccaedfe1823baf3b702da",
+  ];
 
   // console.log("bltx", buffer);
   // console.log("huso", Buffer.from("3797c09006aaad367f7342e215820e499bfbb809f042c690fb7a71b8537c0868", "hex").reverse());
@@ -81,15 +80,13 @@ const getTransactionsById = (buffer?: Buffer, witness?: boolean) => {
     });
 };
 
-const getPeerTransactionsByBlock = (buffer?: Buffer) => {
-  const hashes: Buffer[] = buffer
-    ? [buffer]
-    : [
-        // Buffer.from("538e7f38afa8c6434191c1693a8b30de7cede3a73883f83e7b07c637f2109005", "hex").reverse(),
-        // Buffer.from("f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16", "hex").reverse(),
-        // Buffer.from("3797c09006aaad367f7342e215820e499bfbb809f042c690fb7a71b8537c0868", "hex").reverse(),
-        Buffer.from("1d2362fba0bd11cabdae3e080dad5f0f4db43799052ccaedfe1823baf3b702da", "hex").reverse(),
-      ];
+const getPeerTransactionsByBlock = (blockHashes?: string[]) => {
+  const hashes: string[] = blockHashes || [
+    "0000000000000000002b0fcdc0bdedcc71fcce092633885628c3b50d43200002",
+    // "f4184fc596403b9d638783cf57adfe4c75c605f6356fbc91338530e9831e9e16",
+    // "3797c09006aaad367f7342e215820e499bfbb809f042c690fb7a71b8537c0868",
+    // "1d2362fba0bd11cabdae3e080dad5f0f4db43799052ccaedfe1823baf3b702da",
+  ];
 
   return peer
     .getTransactionsByBlock(hashes)
@@ -123,12 +120,12 @@ const connectionListener = (socket: Socket) => {
 
     getPeerTransactionsByBlock();
 
-    // getTransactionsById();
+    // getPeerTransactionsByTx();
   });
 };
 
 const testIt = () => {
-  const socket: Socket = connectNet({ port: 8333, host: dnsSeeds[4] }, () => {
+  const socket: Socket = connectNet({ port: 8333, host: dnsSeeds[0] }, () => {
     connectionListener(socket);
   });
 };
--- src/lib/Peer.ts
+++ src/lib/Peer.ts
@@ -18,41 +18,41 @@ export class Peer extends PeerBase {
     return this.send<Header[]>("getheaders", ["headers"], getHeadersParams);
   }
 
-  getBlocks(hashes: Buffer[], merkle = false): Promise<Block[]> {
+  getBlocks(hashes: string[], merkle = false): Promise<Block[]> {
     console.log("getBlocks");
 
     const eventNames = hashes.map((hash) => {
       let eventName = merkle ? "merkleblock" : "block";
-      eventName += `:${hash.toString("base64")}`;
+      eventName += `:${this.bufferHash(hash).toString("base64")}`;
       console.log("getBlocks event", eventName);
       return eventName;
     });
 
-    const inventory: Array<{ type: number; hash: Buffer }> = hashes.map((hash: Buffer) => {
+    const inventory: Array<{ type: number; hash: Buffer }> = hashes.map((hash: string) => {
       return {
         type: merkle ? INVENTORY.MSG_FILTERED_BLOCK : INVENTORY.MSG_BLOCK,
-        hash,
+        hash: this.bufferHash(hash),
       };
     });
 
     return this.send<Block>("getdata", eventNames, inventory);
   }
 
-  getTransactionsByTx(txids: Buffer[], witness = false): Promise<Transaction[]> {
-    const eventNames = txids.map((txid) => {
+  getTransactionsByTx(txids: string[], witness = false): Promise<Transaction[]> {
+    const eventNames = txids.map((txid: string) => {
       const eventName = "tx"; // `tx:${txid.toString("base64")}`;
       console.log("TransactionsById event", eventName);
       return eventName;
     });
 
-    const inventory = txids.map((hash: Buffer) => {
-      return { type: witness ? INVENTORY.MSG_WITNESS_TX : INVENTORY.MSG_TX, hash };
+    const inventory = txids.map((hash: string) => {
+      return { type: witness ? INVENTORY.MSG_WITNESS_TX : INVENTORY.MSG_TX, hash: this.bufferHash(hash) };
     });
 
     return this.send<Transaction>("getdata", eventNames, inventory);
   }
 
-  getTransactionsByBlock(blockHashes: Buffer[]): Promise<Transaction[][]> {
+  getTransactionsByBlock(blockHashes: string[]): Promise<Transaction[][]> {
     return this.getBlocks(blockHashes).then((blocks: Block[]) => blocks.map((block: Block) => block.transactions));
   }
 }
